8dc9c4d1aaa0a9fe5bf1b1f25ad82641806f4e61
writesbytes <30513965+writesbytes@users.noreply.github.com>
2021-02-12 18:54:46 +0800

    add sidebar items

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,25 @@
+//module.exports = {
+//  someSidebar: {
+//    Docusaurus: ['home', 'getting-started', 'doc1', 'doc2', 'doc3'],
+//    Features: ['mdx'],
+//  },
+//};
+
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['home', 'getting-started', 'doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-  },
+  doc: [
+    {
+      type: 'doc',
+      label: 'Home',
+    },
+    {
+      type: 'category',
+      label: 'User Guide',
+      items: ['install=mobile=apps', 'create-phonein-account', 'using-mobile=apps'],
+    },
+    {
+      type: 'category',
+      label: 'Installation Guide',
+      items: ['overview', 'installing-phonein', 'connecting-to-a-strike-lock'],
+    },
+  ],
 };
