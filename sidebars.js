5bd8bc5904e50ea66c9766da3a6e6cf1390acf6c
writesbytes <30513965+writesbytes@users.noreply.github.com>
2021-02-12 21:17:57 +0800

    update links

--- sidebars.js
+++ sidebars.js
@@ -1,31 +1,41 @@
-//module.exports = {
-//  someSidebar: {
-//   Docusaurus: ['home', 'getting-started', 'doc1', 'doc2', 'doc3'],
-//   Features: ['mdx'],
-//  },
-//};
+module.exports = {
+  someSidebar: {
+   Docusaurus: ['home', 'getting-started', 'doc1', 'doc2', 'doc3'],
+   Features: ['mdx'],
+  },
+};
 
 module.exports = {
-  docs: [
-    {
-      type: 'doc',
-      label: 'Home',
-      item: ['home'],
-    },
-    {
-      type: 'doc',
-      label: 'Getting Started',
-      item: ['getting-started'],
-    },
-    {
-      type: 'category',
-      label: 'Installation Guide',
-      items: ['overview', 'installing-phonein', 'connecting-to-a-strike-lock'],
-    },
-    {
-      type: 'category',
-      label: 'User Guide',
-      items: ['install-mobile-apps', 'create-phonein-account', 'using-mobile-apps'],
-    },
-  ],
+  someSidebar: {
+   'Home': ['home'],
+   'Getting Started': ['getting-started'],
+   'Installation Guide': ['overview', 'installing-phonein', 'connecting-to-a-strike-lock'],
+   'User Guide': ['install-mobile-apps', 'create-phonein-account', 'using-mobile-apps'],
+  },
 };
+
+
+//module.exports = {
+//  docs: [
+//    {
+//      type: 'doc',
+//      label: 'Home',
+//      item: ['home'],
+//    },
+//    {
+//      type: 'doc',
+//      label: 'Getting Started',
+//      item: ['getting-started'],
+//    },
+//    {
+//      type: 'category',
+//      label: 'Installation Guide',
+//      items: ['overview', 'installing-phonein', 'connecting-to-a-strike-lock'],
+//    },
+//    {
+//      type: 'category',
+//      label: 'User Guide',
+//      items: ['install-mobile-apps', 'create-phonein-account', 'using-mobile-apps'],
+//    },
+//  ],
+//};
