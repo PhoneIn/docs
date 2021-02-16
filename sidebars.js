e0e4495ffddac450ff5ca7bd9050d54866fbb6c9
writesbytes <30513965+writesbytes@users.noreply.github.com>
2021-02-16 12:45:35 +0800

    Update categories, clean up

--- sidebars.js
+++ sidebars.js
@@ -1,50 +1,8 @@
 module.exports = {
   someSidebar: {
-   Docusaurus: ['home', 'getting-started', 'doc1', 'doc2', 'doc3'],
-   Installation Guide: ['overview', 'insalling-phonein', 'connecting-to-a-strike-lock'],
-   User Guide: ['install-apps', 'create-account', 'using-apps'],
+   Docusaurus: ['home', 'getting-started'],
+   Installation_Guide: ['overview', 'insalling-phonein', 'connecting-to-a-strike-lock'],
+   User_Guide: ['install-apps', 'create-account', 'using-apps'],
 //   Features: ['mdx'],
   },
 };
-
-//module.exports = {
-//  someSidebar: {
-//      type: 'doc',
-//      label: 'Home',
-//      id: 'home',
-//   }
-//    {
-//      type: 'doc',
-//      label: 'Getting Started',
-//      id: 'getting-started',
-//    }
-//   User_Guide: ['install-mobile-apps', 'create-phonein-account', 'using-mobile-apps'],
-//   Installation_Guide: ['overview', 'installing-phonein', 'connecting-to-a-strike-lock'],
-//  },
-//};
-
-
-//module.exports = {
-//  docs: [
-//    {
-//      type: 'doc',
-//      label: 'Home',
-//      item: ['home'],
-//    },
-//    {
-//      type: 'doc',
-//      label: 'Getting Started',
-//      item: ['getting-started'],
-//    },
-//    {
-//      type: 'category',
-//      label: 'Installation Guide',
-//      items: ['overview', 'installing-phonein', 'connecting-to-a-strike-lock'],
-//    },
-//    {
-//      type: 'category',
-//      label: 'User Guide',
-//      items: ['install-mobile-apps', 'create-phonein-account', 'using-mobile-apps'],
-//    },
-//  ],
-//};
