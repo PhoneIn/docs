15df3dc93fd3d0ddba99a1ab51ad9ce2ca1486bc
writesbytes <30513965+writesbytes@users.noreply.github.com>
2021-02-16 12:49:40 +0800

    Update categories

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,7 @@
 module.exports = {
   someSidebar: {
    Docusaurus: ['home', 'getting-started'],
-   Installation_Guide: ['overview', 'insalling-phonein', 'connecting-to-a-strike-lock'],
-   User_Guide: ['install-apps', 'create-account', 'using-apps'],
-//   Features: ['mdx'],
+   'Installation Guide': ['overview', 'insalling-phonein', 'connecting-to-a-strike-lock'],
+   'User Guide': ['install-apps', 'create-account', 'using-apps'],
   },
 };
