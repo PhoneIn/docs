ab4ef15e19b9287aade4448ac7d7d3f3a10e3714
writesbytes <30513965+writesbytes@users.noreply.github.com>
2021-02-16 12:55:40 +0800

    Update links for sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
-   Docusaurus: ['home', 'getting-started'],
+   'Home': ['home'], 
+   'Getting Started': ['getting_started'],
    'Installation Guide': ['overview', 'insalling-phonein', 'connecting-to-a-strike-lock'],
    'User Guide': ['install-apps', 'create-account', 'using-apps'],
   },
