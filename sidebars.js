2259fc845551c8096331c8dee7908a7b9b3e0a17
writesbytes <writesbytes@gmail.com>
2021-03-08 16:34:02 +0000

    Update sidebars.js
    Fix links in sidebar

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     'Home': ['home'],
     'Getting Started': ['getting-started'],
-    'Installation Guide': ['Installation Guide/overview', 'Installation Guide/installing-phonein', 'Installation Guide/connecting-to-a-strike-lock'],
-    'User Guide': ['User Guide/install-apps', 'User Guide/create-account', 'User Guide/using-apps'],
+    'Installation Guide': ['InstallationGuide/overview', 'InstallationGuide/installing-phonein', 'InstallationGuide/connecting-to-a-strike-lock'],
+    'User Guide': ['UserGuide/install-apps', 'UserGuide/create-account', 'UserGuide/using-apps'],
   },
 };
